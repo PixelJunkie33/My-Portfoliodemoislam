c3ca3f97803ed606f740721194e5c91e6001cbf8
foadtkf <fakhrulif@gmail.com>
2022-06-09 11:14:31 +0600

    installed daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["bumblebee",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
