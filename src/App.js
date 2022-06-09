c79ce3709e13ba26f0f3c0b86680896addf7ef3b
foadtkf <fakhrulif@gmail.com>
2022-06-09 16:38:20 +0600

    skills page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import Footer from './components/Shared/Footer';
 import { ContactUs } from './components/contactForm/ContactMe';
+import Portfolio from './components/Skills/Skills';
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <Navbar/>
       <Routes >
         <Route path='/' element={<Home/>}/>
+        <Route path='/skills' element={<Portfolio/>}/>
         <Route path='/contactme' element={<ContactUs/>}/>
+
       </Routes>
       <Footer/>
     </div>
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,90 @@
+import React from "react";
+const Portfolio = () => {
+  const frameworks = [
+    "Bootstrap",
+    "Tailwind CSS",
+    "ReactJS",
+    "NodeJS",
+    "Swing",
+  ];
+  const others=[
+      "DaisyUI",
+      "React Reveal",
+      "Stripe",
+  ]
+  const languages = ["C/C++", "Java", "Python", "Javascript"];
+  const tools = ["Firebase", "Git", "Heeroku", "Netlify"];
+  const db = ["MongoDB", "MySQL"];
+  return (
+    <div>
+            <h1 className='text-3xl font-serif font-bold text-center'>Skills & Technologies</h1>
+        <div className="collapse rounded-lg my-5 mx-3  shadow-lg hover:shadow-teal-200">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-gradient-to-r from-violet-900 to-white  text-secondary-content peer-checked:bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked:text-secondary-content text-xl font-bold">
+            Programming Languages
+          </div>
+          <div className="peer-checked:pt-2 collapse-content bg-transparent text-primary-content peer-checked:bg-gradient-to-r from-cyan-500 to-white peer-checked:text-secondary-content">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+              {languages.map((skill) => (
+                <div className="btn">{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="collapse rounded-lg my-5 mx-3  shadow-lg hover:shadow-teal-200">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-gradient-to-r from-violet-900 to-white  text-secondary-content peer-checked:bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked:text-secondary-content text-xl font-bold">
+            Frameworks & Libraries
+          </div>
+          <div className="peer-checked:pt-2 collapse-content bg-transparent text-primary-content peer-checked:bg-gradient-to-r from-cyan-500 to-white peer-checked:text-secondary-content">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+              {frameworks.map((skill) => (
+                <div className="btn">{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="collapse rounded-lg my-5 mx-3  shadow-lg hover:shadow-teal-200">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-gradient-to-r from-violet-900 to-white  text-secondary-content peer-checked:bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked:text-secondary-content text-xl font-bold">
+            Tools
+          </div>
+          <div className="peer-checked:pt-2 collapse-content bg-transparent text-primary-content peer-checked:bg-gradient-to-r from-cyan-500 to-white peer-checked:text-secondary-content">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+              {tools.map((skill) => (
+                <div className="btn">{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="collapse rounded-lg my-5 mx-3  shadow-lg hover:shadow-teal-200">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-gradient-to-r from-violet-900 to-white  text-secondary-content peer-checked:bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked:text-secondary-content text-xl font-bold">
+            Database
+          </div>
+          <div className="peer-checked:pt-2 collapse-content bg-transparent text-primary-content peer-checked:bg-gradient-to-r from-cyan-500 to-white peer-checked:text-secondary-content">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+              {db.map((skill) => (
+                <div className="btn">{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="collapse rounded-lg my-5 mx-3  shadow-lg hover:shadow-teal-200">
+          <input type="checkbox" className="peer" />
+          <div className="collapse-title bg-gradient-to-r from-violet-900 to-white  text-secondary-content peer-checked:bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked:text-secondary-content text-xl font-bold">
+            Others
+          </div>
+          <div className="peer-checked:pt-2 collapse-content bg-transparent text-primary-content peer-checked:bg-gradient-to-r from-cyan-500 to-white peer-checked:text-secondary-content">
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-4">
+              {others.map((skill) => (
+                <div className="btn">{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Portfolio;
